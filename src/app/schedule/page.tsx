3fb2bb3c090c155e756d280a31b7583d12805d76
Katherine Guhl <119278538+katherineguhl@users.noreply.github.com>
2025-04-07 22:26:49 -0400

    I updated the styling on the schedule page to match our figma

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -1,11 +1,15 @@
 import ScheduleSelector from "@/components/ScheduleSelector";
-// UserId is hard coded for now. It can change once auth and sign in/login are done.
-// This references the test user I made.
+
 export default function SchedulePage() {
   return (
-    <div>
-      <h1>My Schedule</h1>
-      <ScheduleSelector userId="67f4563e32a1efe36468fe5a" />
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <div className="border-4 border-red-700 p-10 shadow-xl w-full max-w-4xl text-center">
+        <h1 className="text-3xl font-semibold mb-6 text-red-700">My UGA Schedule</h1>
+        <p className="mb-6 text-lg">
+          Select the days and times of your classes to help us create your personalized schedule.
+        </p>
+        <ScheduleSelector userId="67f4563e32a1efe36468fe5a" />
+      </div>
     </div>
   );
 }
