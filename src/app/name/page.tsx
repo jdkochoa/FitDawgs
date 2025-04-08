653e2d0985da750a6c72d66d879892d757e4a9da
aac69093 <aac69093@uga.edu>
2025-04-08 16:24:28 -0400

    Routing of splash page to name page to schedule page

--- src/app/name/page.tsx
+++ src/app/name/page.tsx
@@ -5,6 +5,10 @@ export default function NamePage() {
 
     const router = useRouter();
 
+    const goToSchedule = () => {
+        router.push("/schedule");
+      };
+
     return (
         <div className="min-h-screen flex items-start justify-center pt-10 bg-white">
             <div className="border-4 border-red-700 p-10 shadow-xl w-full max-w-md text-center">
@@ -44,7 +48,8 @@ export default function NamePage() {
                     className="flex items-center border-2 border-red-600 text-red-600 px-4 py-2 font-semibold hover:bg-red-50">
                         ← Back
                     </button>
-                    <button className="flex items-center bg-red-600 text-white px-6 py-2 font-semibold hover:bg-red-700">
+                    <button className="flex items-center bg-red-600 text-white px-6 py-2 font-semibold hover:bg-red-700"
+                    onClick={goToSchedule}>
                         Next →
                     </button>
                 
