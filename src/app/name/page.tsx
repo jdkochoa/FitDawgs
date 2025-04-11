0b7d4a46f42a944c41489b8f49ea2c2a06611607
Katherine Guhl <119278538+katherineguhl@users.noreply.github.com>
2025-04-11 14:29:56 -0400

    Routing change

--- src/app/name/page.tsx
+++ src/app/name/page.tsx
@@ -25,7 +25,9 @@ export default function NamePage() {
                     className="flex items-center border-2 border-red-600 text-red-600 px-4 py-2 font-semibold hover:bg-red-50">
                         ← Back
                     </button>
-                    <button className="flex items-center bg-red-600 text-white px-6 py-2 font-semibold hover:bg-red-700">
+                    <button
+                    onClick={() => router.push("/schedule")}
+                    className="flex items-center bg-red-600 text-white px-6 py-2 font-semibold hover:bg-red-700">
                         Next →
                     </button>
                 
