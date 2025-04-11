d932ae237c833f1e07122ed1fb69854bce81fea0
Katherine Guhl <119278538+katherineguhl@users.noreply.github.com>
2025-04-11 14:20:50 -0400

    Changed routing

--- src/components/ScheduleSelector.tsx
+++ src/components/ScheduleSelector.tsx
@@ -111,10 +111,11 @@ export default function ScheduleSelector({ userId }: { userId: string }) {
             Back to Home
           </button>
           <button
+            onClick={() => router.push("/calendar")}
             type="submit"
             className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-6 rounded-full"
           >
-            Save Schedule
+            Create Account
           </button>
         </div>
       </form>
