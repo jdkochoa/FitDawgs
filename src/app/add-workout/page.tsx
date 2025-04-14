1fa918271ebd6631fb5290259de6dda11fb2e35b
Katherine Guhl <119278538+katherineguhl@users.noreply.github.com>
2025-04-14 18:07:42 -0400

    add workout page and functionality complete without DB access

---
+++ src/app/add-workout/page.tsx
@@ -0,0 +1,15 @@
+//add a new individual workout
+"use client";
+
+import AddWorkoutForm from "@/components/AddWorkoutForm";
+
+export default function AddWorkoutPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <div className="border-4 border-red-700 p-10 shadow-xl w-full max-w-4xl text-center">
+        <h1 className="text-2xl font-semibold mb-6 text-red-700">Add a New Workout</h1>
+        <AddWorkoutForm />
+      </div>
+    </div>
+  );
+}
