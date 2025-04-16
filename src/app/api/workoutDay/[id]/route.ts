d99086b988aa145e7fca5b9c4a3f036d7825ed43
aac69093 <aac69093@uga.edu>
2025-04-16 15:40:20 -0400

    Workout Day DELETE works by finding the day object's id

--- src/app/api/workoutDay/[id]/route.ts
+++ src/app/api/workoutDay/[id]/route.ts
@@ -1,19 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectMongoDB from "@/lib/mongodb";
 import WorkoutPlan from "@/models/workoutPlan";
+import mongoose from "mongoose";
 
-export default async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-  
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return NextResponse.json({ message: "Invalid ID format" }, { status: 400 });
+  }
+
   await connectMongoDB();
 
   try {
     const result = await WorkoutPlan.updateOne(
-        { "exercises.exercises._id": params.id },
-        { $pull: { "exercises.$[].exercises": { _id: params.id } } }
+      { "exercises._id": id },
+      { $pull: { exercises: { _id: id } } }
     );
 
-    return NextResponse.json({ message: "Workout deleted", result }, { status: 200 });
+    if (result.modifiedCount === 0) {
+      return NextResponse.json({ message: "Workout day not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Workout day deleted", result }, { status: 200 });
   } catch (err) {
-    return NextResponse.json({ message: "Error deleting workout", error: err }, { status: 500 });
+    return NextResponse.json({ message: "Error deleting workout day", error: err }, { status: 500 });
   }
 }
